c499fa83d8293d1f71c187be6c4f4dead94d1ff3
mayihua <mayihua2005@126.com>
2024-11-29 16:20:42 +0800

    update test case

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 // sssss
 int main() {
-  const int a = 9 + 10;
-  return a;
+  const int a = 9 + 10, b = 3 + 20, c = 9 * 20;
+  const int k = 10;
+  int o = k;
+  int p = o + 1;
+  o = o + 1;
+  return a + b / c;
 }
 // int main() { return (-1 + !1) * (+1 - 2); }
 //  ???
