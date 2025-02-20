ab3049bfae23a4d96e30309d24fd59dcd37df3e8
mayihua <mayihua2005@126.com>
2025-02-20 22:04:39 +0800

    updata hello.*

--- hello.c
+++ hello.c
@@ -1,16 +1,24 @@
 // sssss
 
 int main() {
-  int a = 7, b = 9;
-  if (a > b) {
-    a = 8;
+  if (1) {
+    int b = 3;
   }
-  return 1;
-  return 2;
+  int a = 10;
+  if (a > 1)
+    if (a > 2)
+      if (a < 3)
+        return a;
+      else if (a > 4)
+        if (a < 5)
+          return a + 1;
+        else
+          return a + 2;
+  return -1;
 }
 
 // int main() { return (-1 + !1) * (+1 - 2); }
-//  ???
+//   ???
 /* ??? */
 /* ?
  * Helllo
