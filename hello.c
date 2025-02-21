71148f841482241156d9b6859e95711af3701ec5
mayihua <mayihua2005@126.com>
2025-02-21 21:26:35 +0800

    updata hello.*

--- hello.c
+++ hello.c
@@ -1,19 +1,30 @@
 // sssss
 
 int main() {
-  if (1) {
-    int b = 3;
+  while (1) {
+    int a = 1, b = 2;
+    {
+      if (a == 1) {
+        while (a < b) {
+          while (a < b || b - 1 == 0) {
+            a = a + 1;
+          }
+          b = 1;
+          a = a + 1;
+          if (3)
+            continue;
+        }
+      } else if (b == 6) {
+        break;
+      }
+      int b = 6;
+      if (b == 6)
+        return 8 * (10 || b);
+      else
+        while (0)
+          ;
+    }
   }
-  int a = 10;
-  if (a > 1)
-    if (a > 2)
-      if (a < 3)
-        return a;
-      else if (a > 4)
-        if (a < 5)
-          return a + 1;
-        else
-          return a + 2;
   return -1;
 }
 
